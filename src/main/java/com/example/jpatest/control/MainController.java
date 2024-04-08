ba1c631a841ddd098d98797de1b24132f810cc5e
User <songjaey88@gmail.com>
2024-04-09 01:11:58 +0900

    d

--- src/main/java/com/example/jpatest/control/MainController.java
+++ src/main/java/com/example/jpatest/control/MainController.java
@@ -11,10 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 
 @Controller
 @RequiredArgsConstructor
@@ -44,23 +46,45 @@ public class MainController {
         return "member/memberForm";
     }
 
-    @PostMapping("members/new")
+    @PostMapping("/members/new")
     public String newMember(@Valid MemberFormDto memberFormDto,
-                            BindingResult bindingResult, Model model){
-        if( bindingResult.hasErrors()  ){
-            return "member/memberForm";
+                            BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            // 필드 검증 오류가 있는 경우
+            System.out.println("validationError");
+            return "member/memberForm"; // 회원가입 폼을 다시 표시
         }
 
-        try{
-            memberService.saveMember(memberFormDto, passwordEncoder );
-        }catch(IllegalStateException e){
-            model.addAttribute("errorMessage", e.getMessage());
-            return "member/memberForm";
+        try {
+            memberService.saveMember(memberFormDto, passwordEncoder);
+        } catch (IllegalStateException e) {
+            // 회원가입 중 예외 발생 시
+            model.addAttribute("emailError", "이미 사용 중인 이메일입니다.");
+            System.out.println("ExceptionError");
+            return "member/memberForm"; // 회원가입 폼을 다시 표시
         }
 
-        return "redirect:/";
+        return "redirect:/"; // 회원가입 성공 시 메인 페이지로 리다이렉트
     }
 
+    @GetMapping("/members/findId")
+    public String findIdGet(Model model){
+        model.addAttribute("tel", ""); // 빈 문자열로 초기화하여 전화번호 필드를 전달
+        return "member/findId";
+    }
+    @PostMapping("/members/findId")
+    public String findIdPost(@NotBlank @RequestParam String tel, Model model) {
+        try {
+            String foundId = memberService.findIdByTel(tel);
+            model.addAttribute("foundId", foundId); // 찾은 아이디를 모델에 추가
+            return "member/foundId"; // 찾은 아이디를 보여주는 뷰로 이동
+        } catch (IllegalStateException e) {
+            // 해당 전화번호로 가입된 회원이 없는 경우
+            model.addAttribute("findIdErrorMsg", "입력한 전화번호로 가입된 회원이 없습니다.");
+            model.addAttribute("tel", tel); // 입력한 전화번호를 다시 모델에 추가
+            return "member/findId"; // 아이디 찾기 폼을 다시 표시
+        }
+    }
 
 }
-    //
+//
